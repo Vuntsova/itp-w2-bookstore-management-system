7fb0785b64fea92e029600f7e5008e651b321db2
vuntsova <evuntsova@gmail.com>
2019-01-31 17:16:33 +0000

    all tests passed

--- bookstore/bookstore.py
+++ bookstore/bookstore.py
@@ -1,26 +1,70 @@
 def create_bookstore(name):
-    pass
-
+    bookstore = {
+        "name": name,
+        'authors' : [],
+        'books': []
+        
+    }
+    return bookstore
+print(create_bookstore("rmotr's bookstore"), " 1")
 
 def get_bookstore_name(bookstore):
-    pass
-
+#     print(bookstore['name'], " 2")
+    return bookstore['name']
+get_bookstore_name(create_bookstore('fm'))
 
 def add_author(bookstore, name, nationality):
-    pass
+    author = {
+        "name": name, 
+        "nationality":nationality,
+        
+    }
+    bookstore['authors'].append(author)
+    return author
 
 
 def get_author_by_name(bookstore, name):
-    pass
+    for author in bookstore['authors']:
+        if author['name'] == name:
+            return author
+# print(get_author_by_name(create_bookstore("rmotr's bookstore"), 'James Joyce'), "get_author_by_name")
 
 
 def add_book(bookstore, title, isbn, author):
-    pass
+    book = { 
+        'title':title, 
+        'isbn':isbn, 
+        'author':author
+    }
+    
+    bookstore['books'].append(book )
+    return book
+    
+    
+    
+#     for a in bookstore['authors']:
+#         if a['name'] == author:
+#             a.setdefault('title', title)
+#             a.setdefault('isbn', isbn)
+#             return a
+print()
+print()
+print()
+print(add_book(create_bookstore("rmotr's bookstore"), 'El Aleph', 'XXX-4', 'Jorge Luis Borges'))
+    
 
 
 def get_book_by_title(bookstore, title):
-    pass
+    for book in bookstore['books']:
+        if book['title'] == title:
+            return book
 
 
 def get_books_by_author(bookstore, author):
-    pass
+    books = []
+    for book in bookstore['books']:
+        if book['author'] == author:
+            books.append(book)
+        
+    return books
+    
